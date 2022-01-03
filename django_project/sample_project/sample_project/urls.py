c4de97b15d2d9d244e35308bb1561626890aaf3d
gm4vin2tech <gm4vin2tech@gmail.com>
2022-01-03 19:38:51 +0530

    sample project

--- django_project/sample_project/sample_project/urls.py
+++ django_project/sample_project/sample_project/urls.py
@@ -1,4 +1,4 @@
-"""test_project URL Configuration
+"""sample_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -16,14 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home, demo
-from testapp.views import que
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name="home" ),
-    path('demo', demo, name="demo" ),
-    path('que', que, name="que" ),
-
+    path('', home, name='home' ),
+    path('demo', demo, name='demo' ),
 
 ]
