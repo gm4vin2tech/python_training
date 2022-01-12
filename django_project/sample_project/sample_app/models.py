779a3db5b5b724dd10ace60179dc1fce6755e546
gm4vin2tech <gm4vin2tech@gmail.com>
2022-01-12 20:10:03 +0530

    admin-app

---
+++ django_project/sample_project/sample_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Teacher(models.Model):
+	name = models.CharField(max_length=50)
+
+class Destination(models.Model):
+	name = models.CharField(max_length=100)
+	desc = models.TextField()
+	price = models.IntegerField()
+	offer = models.BooleanField(default=False)	
+
